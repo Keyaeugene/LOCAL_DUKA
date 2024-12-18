8d9c558f1e76fbb53c7e1cce6d8c997e2465b0b8
keyaeugene <keyaeugene@gmail.com>
2024-12-18 20:47:04 +0300

    debugging

---
+++ lib/features/account/widgets/below_app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce/provider/user_provider.dart';
+import 'package:provider/provider.dart';
+import '../../../constants/global_variables.dart';
+
+class BelowAppBar extends StatelessWidget {
+  const BelowAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).user;
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: GlobalVariables.appBarGradient,
+      ),
+      padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+      child: RichText(
+        text: TextSpan(
+            text: 'Hello',
+            style: TextStyle(
+              fontSize: 22,
+              color: Colors.black,
+            ),
+            children: [
+              TextSpan(
+                text: user.name,
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.black,
+                ),
+              )
+            ]),
+      ),
+    );
+  }
+}
