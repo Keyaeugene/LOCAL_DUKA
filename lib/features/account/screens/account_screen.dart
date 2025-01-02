7c0e9da5aa59a7697dd66d1924b6fe5f6b2c989b
keyaeugene <keyaeugene@gmail.com>
2025-01-02 14:38:07 +0300

    debugging

--- lib/features/account/screens/account_screen.dart
+++ lib/features/account/screens/account_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce/features/account/widgets/below_app_bar.dart';
 import '../../../constants/global_variables.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -7,43 +8,47 @@ class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(50),
-        child: AppBar(
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              gradient: GlobalVariables.appBarGradient,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(50),
+          child: AppBar(
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                gradient: GlobalVariables.appBarGradient,
+              ),
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: Image.asset(
+                    'assets/images/amazon_in.png',
+                    width: 120,
+                    height: 45,
+                    color: Colors.black,
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.only(left: 15, right: 15),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 15),
+                          child: Icon(Icons.notifications_outlined),
+                        ),
+                        Icon(
+                          Icons.search,
+                        )
+                      ],
+                    ))
+              ],
             ),
           ),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                alignment: Alignment.topLeft,
-                child: Image.asset(
-                  'assets/images/amazon_in.png',
-                  width: 120,
-                  height: 45,
-                  color: Colors.black,
-                ),
-              ),
-              Container(
-                  padding: const EdgeInsets.only(left: 15, right: 15),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(right: 15),
-                        child: Icon(Icons.notifications_outlined),
-                      ),
-                      Icon(
-                        Icons.search,
-                      )
-                    ],
-                  ))
-            ],
-          ),
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            BelowAppBar(),
+          ],
+        ));
   }
 }
